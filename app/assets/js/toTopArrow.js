784c46996b6192a6ab718ec54b51473d52d03421
AndSmi <andsmidev@gmail.com>
2020-11-29 22:29:13 +0300

    Added top arrow

---
+++ app/assets/js/toTopArrow.js
@@ -0,0 +1,15 @@
+;
+(function(){
+  const arrowBtn = document.querySelector('.to-top');
+
+  window.addEventListener('scroll', () => {
+    const scrolled = window.pageYOffset;
+
+    if(scrolled > 100) {
+      arrowBtn.classList.add('to-top--active');
+    }
+    else {
+      arrowBtn.classList.remove('to-top--active');
+    }
+  });
+})();
