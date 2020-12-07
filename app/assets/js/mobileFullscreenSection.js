0ff72001e5095dce9a1dfa618e76f430f72a6b4a
AndSmi <andsmidev@gmail.com>
2020-12-07 20:10:04 +0300

    Renamed a callback function

--- app/assets/js/mobileFullscreenSection.js
+++ app/assets/js/mobileFullscreenSection.js
@@ -1,11 +1,11 @@
 ;
 (function () {
-  const giveMeAdaptivePls = () => {
+  const giveMeFullScreenPls = () => {
     const vh = window.innerHeight * .01;
 
     document.documentElement.style.setProperty('--vh', vh + 'px');
   };
 
-  window.addEventListener('DOMContentLoaded', giveMeAdaptivePls);
-  window.addEventListener('resize', giveMeAdaptivePls);
+  window.addEventListener('DOMContentLoaded', giveMeFullScreenPls);
+  window.addEventListener('resize', giveMeFullScreenPls);
 })();
