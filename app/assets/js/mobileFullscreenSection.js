5b88ad08ae77228a38f9d8fcac5827a09b48cc34
AndSmi <andsmidev@gmail.com>
2020-12-07 20:00:00 +0300

    Fixed problems with heights on mobiles

--- app/assets/js/mobileFullscreenSection.js
+++ app/assets/js/mobileFullscreenSection.js
@@ -1,3 +1,6 @@
-const vh = window.innerHeight * .01;
+;
+window.addEventListener('resize', () => {
+  const vh = window.innerHeight * .01;
 
-document.documentElement.style.setProperty('--vh', vh + 'px');
+  document.documentElement.style.setProperty('--vh', vh + 'px');
+});
