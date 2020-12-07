4fb223dc8f2b16e3b1cf7247456cc463bceb32d1
AndSmi <andsmidev@gmail.com>
2020-12-07 20:07:13 +0300

    Added fixes for mobile resizing fullscreen blocks

--- app/assets/js/mobileFullscreenSection.js
+++ app/assets/js/mobileFullscreenSection.js
@@ -1,6 +1,11 @@
 ;
-window.addEventListener('resize', () => {
-  const vh = window.innerHeight * .01;
+(function () {
+  const giveMeAdaptivePls = () => {
+    const vh = window.innerHeight * .01;
 
-  document.documentElement.style.setProperty('--vh', vh + 'px');
-});
+    document.documentElement.style.setProperty('--vh', vh + 'px');
+  };
+
+  window.addEventListener('DOMContentLoaded', giveMeAdaptivePls);
+  window.addEventListener('resize', giveMeAdaptivePls);
+})();
