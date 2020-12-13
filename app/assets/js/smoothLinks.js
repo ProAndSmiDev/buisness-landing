6f78bbf982f54c64cee43656f302b904ac372400
AndSmi <andsmidev@gmail.com>
2020-12-13 16:52:39 +0300

    Added global styles for picture, updated js for links and styles for logo, updated mixin for menu

--- app/assets/js/smoothLinks.js
+++ app/assets/js/smoothLinks.js
@@ -1,5 +1,20 @@
 ;
-(function(){
+(function () {
+  const toggleActiveLink = (el) => {
+    const menus = document.querySelectorAll('.menu');
+
+    menus.forEach((m) => {
+      const menuLink = m.querySelectorAll('.menu__link');
+
+      menuLink.forEach((mLink) => {
+        mLink.classList.remove('menu__link--active');
+
+        if (mLink.innerText && mLink.innerText === el.innerText) {
+          mLink.classList.add('menu__link--active');
+        }
+      });
+    });
+  };
   const allLinks = document.querySelectorAll('a[href^="#"]');
 
   allLinks.forEach((link, i) => {
@@ -9,9 +24,15 @@
       const blockID = link.getAttribute('href').substr(1);
       const blockEl = document.getElementById(blockID).offsetTop;
 
+      console.log(link.innerText, link);
+
       (window.innerWidth >= 768)
-        ? window.scrollTo({top: blockEl - 85, behavior:'smooth'})
-        : window.scrollTo({top: blockEl - 56, behavior:'smooth'});
+        ? window.scrollTo({top: blockEl - 85, behavior: 'smooth'})
+        : window.scrollTo({top: blockEl - 56, behavior: 'smooth'});
+
+      if (link.closest('.menu')) {
+        toggleActiveLink(link);
+      }
     });
   });
 })();
